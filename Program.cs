5fb8104f1d8f07996bb32d41efbd8016e5971db0
alenka2507 <alena@elkursk.ru>
2025-03-13 16:00:16 +0300

    привет, мир

--- Program.cs
+++ Program.cs
@@ -4,9 +4,8 @@
     {
         static void Main(string[] args)
         {
-            string MyName;
-            MyName = "Алёна";
-            Console.WriteLine(MyName);
+            Console.WriteLine("Привет, \nмир");
+                     Console.ReadKey();
         }
     }
 }
