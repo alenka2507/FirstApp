16c268bfc2cf13f9fa9d7a828a2661e330a68f15
alenka2507 <alena@elkursk.ru>
2025-03-13 15:09:13 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Module3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+            MyName = "Алёна";
+            Console.WriteLine(MyName);
+        }
+    }
+}
